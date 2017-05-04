2ca4cf9a9c1a434efacc4c16f9125f2d2567c88c
Insid1ouZ <rasmus.eriksson324@gmail.com>
2017-05-04 13:05:20 +0200

    FPS Game
    An FPS Game

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Bullet : MonoBehaviour {
+
+	public GameObject Bullet_prefab;
+	public GameObject Granade_prefab;
+	public float Bulletimpulse;
+	public float Granadeimpulse;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			Camera cam = Camera.main;
+			GameObject thebullet = (GameObject)Instantiate (Bullet_prefab, cam.transform.position, cam.transform.rotation);
+			thebullet.GetComponent<Rigidbody>().AddForce(cam.transform.forward * Bulletimpulse, ForceMode.Impulse);
+
+			Object.Destroy(thebullet, 20.0f);
+		}
+
+		if (Input.GetButtonDown ("Fire2")) 
+		{
+			Camera cam = Camera.main;
+			GameObject Granade = (GameObject)Instantiate (Granade_prefab, cam.transform.position, cam.transform.rotation);
+			Granade.GetComponent<Rigidbody>().AddForce(cam.transform.forward * Granadeimpulse, ForceMode.Impulse);
+
+
+		}
+
+
+
+
+		}
+	}
+
---
+++ Assets/Scripts/pausemenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausemenu : MonoBehaviour  {
+    public Transform PauseMenu;
+    public characterControll cc;
+    public Bullet bullet;
+
+    void Start()
+    {
+        cc = GetComponent<characterControll>();
+        bullet = GetComponent<Bullet>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(PauseMenu.gameObject.activeInHierarchy == false)
+            {
+                PauseMenu.gameObject.SetActive(true);
+                Cursor.visible = true;
+                cc.enabled = false;
+                bullet.enabled = false;
+            } else {
+                PauseMenu.gameObject.SetActive(false);
+                Cursor.visible = false;
+                cc.enabled = true;
+                bullet.enabled = true;
+            }
+        }
+		
+	}
+}
